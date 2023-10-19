c03d20bdb2564053522a93d0695dfc0bbd1a2ce5
alexobviously <iamalexbaker@gmail.com>
2023-10-19 17:09:29 +0000

    Deploying to gh-pages from @ goodpals/didyoucodetoday@fa0365b39b64ad6cebae1a2db44b4ebb197d63ee 🚀

--- android/app/src/main/kotlin/com/example/didyoucodetoday/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.didyoucodetoday
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/date.dart
+++
@@ -1,6 +0,0 @@
-int nowMs() => DateTime.now().millisecondsSinceEpoch;
-const oneDayMs = 1000 * 60 * 60 * 24;
-
-extension RoundDownToDay on DateTime {
-  DateTime roundDownToDay() => DateTime(year, month, day);
-}
